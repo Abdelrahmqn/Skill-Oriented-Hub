7af73af88dec0d44489d471d3a4a93b9b803a540
jawkat <kad.joliano@gmail.com>
2024-10-12 02:07:24 +0100

    Handling User Management part two
    ## Details
    - Implemented reset password functionality.
    
    ## Related Issues
    - nothing
    
    ## Changes Made
    - add function  get_reset_token and verify_reset_token to the user model

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,10 +1,10 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
 from flask_login import login_user, logout_user, login_required, current_user
+from flask_mail import Message
 from wtforms.fields.choices import RadioField
-
-from app import db
-from app.models import User
 from werkzeug.security import generate_password_hash
+from app import db, mail
+from app.models import User
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Email, EqualTo, ValidationError
@@ -116,7 +116,7 @@ class RequestResetForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if user is None:
-            raise ValidationError('There is no account with that email. You must register first.')
+            raise ValidationError('There is no account with that email.')
 
 
 class ResetPasswordForm(FlaskForm):
@@ -124,3 +124,45 @@ class ResetPasswordForm(FlaskForm):
     confirm_password = PasswordField('Confirm Password',
                                     validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset Password')
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                sender='noreply@demo.com',
+                recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('auth.reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+
+@bp.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_request.html', form=form)
+
+
+@bp.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        flash('That is an invalid or expired token', 'warning')
+        return redirect(url_for('auth.reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        user.set_password(form.password.data)
+        db.session.commit()
+        flash('Your password has been updated! You are now able to log in', 'success')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/reset_token.html', form=form)
--- config.py
+++ config.py
@@ -4,3 +4,11 @@ class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'mysecretkey')
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///e_learning.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_DEFAULT_SENDER = 'noreply@demo.com'
+
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME', 'pic3promo@gmail.com')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', 'ukns hlfx kixi ydxq')
